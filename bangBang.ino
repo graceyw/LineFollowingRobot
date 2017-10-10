bd76c990c1094b19bf8afe75e253ec724ff76cb0
Gracey Wilson <gcwilson87@gmail.com>
2017-10-10 12:50:28 -0400

    Add files via upload

---
+++ bangBang.ino
@@ -0,0 +1,16 @@
+void setup() {
+  // put your setup code here, to run once:
+  #include <Servo.h>
+  Servo rightWheel;        //create servo object to control right wheel
+  Servo leftWheel;         //create servo object to control left wheel
+  int rightSensor = analogRead(A0);
+  int leftSensor = analogRead(A1);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println(analogRead(A0));
+//  Serial.print(', ');
+  Serial.println(analogRead(A1));
+}
+
