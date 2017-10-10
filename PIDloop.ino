e077d65290beb1e4f65449a998e65fdf49ac8634
Gracey Wilson <gcwilson87@gmail.com>
2017-10-10 12:51:38 -0400

    Add files via upload

---
+++ PIDloop.ino
@@ -0,0 +1,23 @@
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+#include "utility/Adafruit_MS_PWMServoDriver.h"
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+
+Adafruit_DCMotor *leftMotor = AFMS.getMotor(1);
+Adafruit_DCMotor *rightMotor = AFMS.getMotor(1);    //using port number i.e. M1=1, M4=4 etc.
+int rightSensor = analogRead(A0);
+int leftSensor = analogRead(A1);
+
+void setup() {
+  AFMS.begin();
+  rightMotor->setSpeed(150);   //initialize right motor speed
+  leftMotor->setSpeed(150);    //initialize left motor speed
+
+  Serial.begin(9600);          //initialize Serial moniter
+  Serial.println('Beginning program now.');
+}
+
+void loop() {
+  Serial.println(rightSensor);
+  Serial.println(leftSensor);            //for testing purposes
+}
